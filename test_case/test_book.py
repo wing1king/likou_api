d51333cf9bab898edbe1cb3ba9cf523ed21f3054
peak.zhang@idreamsky.com <zhang4475153@126.com>
2019-10-16 14:49:55 +0800

    平台接口

--- test_case/test_book.py
+++ test_case/test_book.py
@@ -2,7 +2,7 @@ from get_token import *
 
 
 class MyTestCase(unittest.TestCase):
-    """阅读进度"""
+    """书本"""
 
     def setUp(self):
         self.book_id = 129337698467840
@@ -10,115 +10,163 @@ class MyTestCase(unittest.TestCase):
         self.reason_id = 4
 
 
-    def test_report(self):
-        """投拆"""
-        data = data
-        res = requests.post(url= base_url + "/app/user/report",headers=headers,json=data)
+    def test_h5_file(self):
+        """获取-book-H5"""
+        data = {
+            "book_id": self.book_id
+        }
+        res = requests.post(url= base_url + "/app/book/h5-file",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_login(self):
-        """登录/注册"""
+    def test_book_file(self):
+        """获取播放文件"""
         data = {
-	        "book_id": self.book_id,
-	        "chapter_id": self.chapter_id,
-	        "cmd_id": self.cmd_id,
-	        "option_id": self.option_id
+	        "book_id": self.book_id
 	        }
-        res = requests.post(url= base_url + "/app/user/login",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/book-file",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_refresh_token(self):
-        """刷新token"""
+    def test_list(self):
+        """获取首页书本列表"""
         data = {
-	        "access_token": get_token()
+	        "page_num": 1,
+            "page_size": 5
             }
-        res = requests.post(url= base_url + "/app/user/refresh-token",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/list",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_black_list(self):
-        """屏蔽用户"""
+    def test_list_tag(self):
+        """获取标签书本列表"""
         data = {
-	        "report_user_id": 2264982076
-	        }    
-        res = requests.post(url= base_url + "/app/add-black-list",headers=headers,json=data)
+	        "cate_id": 2,
+	        "page_num": 1,
+	        "page_size": 5,
+	        "order": 0
+            }
+        res = requests.post(url= base_url + "/app/book/list-tag",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_books(self):
-        """书本列表"""
+    def test_recommends(self):
+        """获取推荐作品列表"""
         data = {
 	        "page_num": 1,
 	        "page_size": 5
             }
-        res = requests.post(url= base_url + "/app/user/books",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/recommends",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_edit_profile(self):
-        """设置个人数据"""
+    def test_collect_list(self):
+        """获取书本收藏列表"""
         data = {
-	        "nick_name": "wingking",
-            "desc": "",
-            "avatar": "https://xms-dev-1251001060.cos.ap-guangzhou.myqcloud.com/2019/09/1568086837108.jpg"
+	        "page_num": 1,
+	        "page_size": 5
             }
-        res = requests.post(url= base_url + "/app/user/edit-profile",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/collect-list",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_get_profile(self):
-        """获取个人页数据"""
+    def test_history_list(self):
+        """获取书本浏览记录"""
         data = {
-	        "user_id": self.user_id,
+	        "page_num": 1,
+	        "page_size": 5
             }
-        res = requests.post(url= base_url + "/app/user/get-profile",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/history-list",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)    
 
-    def test_get_black_list(self):
-        """获取黑名单列表"""
+    def test_list_book_id(self):
+        """获取book_id列表"""
         data = {
 	        "page_num": 1,
 	        "page_size": 5
             }
-        res = requests.post(url= base_url + "/app/user/get-black-list",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/list-book-id",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)   
 
-    def test_add_black_list(self):
-        """将用户加入黑名单"""
+    def test_collect(self):
+        """收藏书本"""
         data = {
-	        "report_user_id": self.user_id
+	        "book_id": self.book_id,
+            "type": 0
             }
-        res = requests.post(url= base_url + "/app/user/add-black-list",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/collect",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_cancel_black_list(self):
-        """将用户移出黑名单"""
+    def test_collect1(self):
+        """取消收藏书本"""
         data = {
-	        "report_user_id": self.user_id
+	        "book_id": self.book_id,
+            "type": 1
             }
-        res = requests.post(url= base_url + "/app/user/cancel-black-list",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/collect",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)        
 
-    def test_get_black_list(self):
-        """获取黑名单中指定用户"""
+    def test_praise(self):
+        """点赞书本"""
         data = {
-	        "report_user_id": self.user_id
+	        "book_id": self.book_id
             }
-        res = requests.post(url= base_url + "/app/user/get-black-list",headers=headers,json=data)
+        res = requests.post(url= base_url + "/app/book/praise",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
 
-    def test_get_cos_auth(self):
-        """获取COS临时参数"""
-        res = requests.post(url= base_url + "/app/user/get-cos-auth",headers=headers)
+    def test_praise_cancel(self):
+        """取赞书本"""
+        data = {
+	        "book_id": self.book_id
+            }        
+        res = requests.post(url= base_url + "/app/book/praise-cancel",headers=headers,json=data)
         print(res.text)
         self.assertTrue(u"success" in res.text)
+
+    def test_get_book(self):
+        """作品详情"""
+        data = {
+	        "book_id": self.book_id
+            }           
+        res = requests.post(url= base_url + "/app/book/get-book",headers=headers,json=data)
+        print(res.text)
+        self.assertTrue(u"success" in res.text)
+
+    def test_share(self):
+        """分享上报"""
+        data = {
+            "book_id": self.book_id
+        }        
+        res = requests.post(url= base_url + "/app/book/share",headers=headers,json=data)
+        print(res.text)
+        self.assertTrue(u"success" in res.text)
+
+    def test_list_activity(self):
+        """获取活动相关书本列表"""
+        data = {
+	        "activity_id": "",
+	        "user_id": self.user_id,
+	        "order": 0,	
+	        "page_num": 1,
+	        "page_size": 5
+	    }    
+        res = requests.post(url= base_url + "/app/book/list-activity",headers=headers,json=data)
+        print(res.text)
+        self.assertTrue(u"success" in res.text)
+
+    def test_search_book(self):
+        """搜索书本"""
+        data = {
+            "keyword": ""
+        }
+        res = requests.post(url= base_url + "/app/book/search-book",headers=headers,json=data)
+        print(res.text)
+        self.assertTrue(u"success" in res.text)        
 
     def tearDown(self):
         time.sleep(1)
